ba7ffd867bd1f3326dadbfa96156798af478f1e1
ShaiDiamant <shaidiamant@gmail.com>
2020-01-11 20:26:08 +0200

    derp

--- src/Flight.java
+++ src/Flight.java
@@ -1,10 +1,10 @@
 
 abstract public class Flight implements Runnable {
 
-	protected String flightCode;//TODO: Remove this
+	protected String flightCode;
 	protected int numOfPassengers;
 	protected int arrivalTime;
-	protected Queue<Flight> flightsQ;//TODO: Remove this also
+	protected Queue<Flight> flightsQ;
 	private Object latestTreater;
 	
 	public Flight (String flightCode, int numOfPassengers, int arrivalTime, Queue<Flight> flightsQ) {
