74e01ff24f58ab97020b576fdc6a80c8a89b936c
ShaiDiamant <shaidiamant@gmail.com>
2020-01-11 14:52:39 +0200

    aaa

--- src/Flight.java
+++ src/Flight.java
@@ -1,7 +1,7 @@
 
 abstract public class Flight implements Runnable {
 
-	protected String flightCode;//herpa
+	protected String flightCode;//derp
 	protected int numOfPassengers;
 	protected int arrivalTime;
 	protected Queue<Flight> flightsQ;
