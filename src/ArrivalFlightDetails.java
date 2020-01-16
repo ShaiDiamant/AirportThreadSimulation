ba8535612e08f49732e7a09194438532d8db0a6d
ShaiDiamant <shaidiamant@gmail.com>
2020-01-16 15:46:32 +0200

    derp

--- src/ArrivalFlightDetails.java
+++ src/ArrivalFlightDetails.java
@@ -5,35 +5,29 @@ public class ArrivalFlightDetails extends FlightDetails {
 	private int cost;
 	private int amountOfFuel;
 	private boolean securityIssue;
-	private Vector<FuelCrew> fuelCrews = new Vector<FuelCrew>();
 
 	public ArrivalFlightDetails(String flightCode, int numOfPassengers, int timeInAirfield,
-			int cargo, int cost, boolean securityIssue, Vector<FuelCrew> fuelCrews) {
+								int cargo, int cost, boolean securityIssue) {
 		super(flightCode, numOfPassengers, timeInAirfield);
 		this.cargo = cargo;
 		this.cost = cost;
 		this.securityIssue = securityIssue;
-		this.amountOfFuel=0;
-		this.fuelCrews=fuelCrews;
+		this.amountOfFuel = 0;
 	}
+
 	public int getCost() {
-		return this.cost; 
+		return this.cost;
 	}
-	
+
 	public boolean getSecurityIssue() {
 		return this.securityIssue;
 	}
-	
+
 	public int getCargo() {
 		return this.cargo;
 	}
-	
-	public int getamountOfFuel() {
-		for(int i=0; i<fuelCrews.size(); i++) {
-			FuelCrew curr = fuelCrews.get(i);
-			this.amountOfFuel = this.amountOfFuel + curr.getNumOfFuels()*1000;
-		}
-		return this.amountOfFuel;
-	}
 
+	public int getamountOfFuel() {//TODO: Find solution
+		return 0;
+	}
 }
